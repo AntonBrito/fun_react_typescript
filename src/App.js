d91fefac59123a992bf48aea161e71d29676a244
AntonBrito <tonybrito85@gmail.com>
2020-09-22 14:29:57 -0400

    EDEfining the event callback

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const onSearchInput = (event: React.ChangeEvent<HTMLInputElement>) => {
   
+  }
   return (
     <div className="App">
       <input onChange={onSearchInput} name="search" type="text" />
